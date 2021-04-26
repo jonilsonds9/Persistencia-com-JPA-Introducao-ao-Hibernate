12ff0253a1706b380aaa83597650fac0fbb5e38f
Jonilson Sousa <jonilsonds9@gmail.com>
2021-04-26 09:44:18 -0300

    Estados no delete da entidade

--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -26,6 +26,7 @@ public class CadastroDeProduto {
         celulares = em.merge(celulares);
         celulares.setNome("1234");
         em.flush();
+        em.clear();
 
         em.remove(celulares);
         em.flush();

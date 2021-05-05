eebb7c1a0e153439500655eded4bfbcb5d45b1dd
Jonilson Sousa <jonilsonds9@gmail.com>
2021-05-05 10:14:24 -0300

    Consultando entidades

--- src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -3,6 +3,7 @@ package br.com.alura.loja.dao;
 import br.com.alura.loja.modelo.Produto;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 public class ProdutoDao {
 
@@ -14,6 +15,15 @@ public class ProdutoDao {
 
     public void cadastrar(Produto produto) {
         this.em.persist(produto);
+    }
+
+    public Produto buscarPorId(Long id) {
+        return this.em.find(Produto.class, id);
+    }
+
+    public List<Produto> buscarTodos() {
+        String jpql = "SELECT p FROM Produto p";
+        return em.createQuery(jpql, Produto.class).getResultList();
     }
 
 }
--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -8,27 +8,37 @@ import br.com.alura.loja.util.JPAUtil;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.List;
 
 public class CadastroDeProduto {
 
     public static void main(String[] args) {
+        cadastrarProduto();
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+
+        Produto p = produtoDao.buscarPorId(1L);
+        System.out.println(p.getPreco());
+
+        List<Produto> todos = produtoDao.buscarTodos();
+        todos.forEach(p2 -> System.out.println(p2.getNome()));
+    }
+
+    private static void cadastrarProduto() {
         Categoria celulares = new Categoria("CELULARES");
 
+        Produto celular = new Produto("Xiomi Reami", "Muito legal", new BigDecimal("800"), celulares);
+
         EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+        CategoriaDao categoriaDao = new CategoriaDao(em);
+
         em.getTransaction().begin();
 
-        em.persist(celulares);
-        celulares.setNome("XPTO");
+        categoriaDao.cadastrar(celulares);
+        produtoDao.cadastrar(celular);
+        em.getTransaction().commit();
 
-        em.flush();
-        em.clear();
-
-        celulares = em.merge(celulares);
-        celulares.setNome("1234");
-        em.flush();
-        em.clear();
-
-        em.remove(celulares);
-        em.flush();
+        em.close();
     }
 }
